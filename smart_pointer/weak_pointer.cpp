8285dbcb446770123b54e096da66698011920041
vietmaiquoc <quocvm1@gmail.com>
2021-07-22 01:01:38 +0800

    [Viet]

---
+++ smart_pointer/weak_pointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <memory>
+
+int main()
+{
+    std::shared_ptr<int> mySharedPtr(new int);
+    std::cout << "shared pointer count = " << mySharedPtr.use_count() << std::endl;
+
+    std::weak_ptr<int> myWeakPtr1(mySharedPtr);
+    std::weak_ptr<int> myWeakPtr2(myWeakPtr1);
+    std::cout << "shared pointer count = " << mySharedPtr.use_count() << std::endl;
+
+    return 0;
+
+}
