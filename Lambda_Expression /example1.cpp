2018f238730c712942e763b590ac62135651f2a2
vietWaveboost <vietmq@waveboost.net>
2021-08-30 23:42:36 +0800

    [Viet] lambda fuction study

---
+++ Lambda_Expression /example1.cpp
@@ -0,0 +1,20 @@
+// declaring_lambda_expressions1.cpp
+// compile with: /EHsc /W4
+#include <functional>
+#include <iostream>
+
+int main()
+{
+
+    using namespace std;
+
+    // Assign the lambda expression that adds two numbers to an auto variable.
+    auto f1 = [](int x, int y) { return x + y; };
+
+    cout << f1(2, 3) << endl;
+
+    // Assign the same lambda expression to a function object.
+    function<int(int, int)> f2 = [](int x, int y) { return x + y; };
+
+    cout << f2(3, 4) << endl;
+}
